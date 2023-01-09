0a2fb28230780dac49571fbe787cedc5159d067d
YannNG1 <yann.nunez-guajardo@eleves.ecmorlaix.fr>
2023-01-09 09:50:09 +0000

    t

---
+++ trait.py
@@ -0,0 +1,13 @@
+# Définitions
+
+# Fonction trait()
+def trait(x1,y1,x2,y2):
+    '''
+    dessine un trait entre les 2 points transmis en paramètres
+    Paramètres
+        x1, y1 : coordonnées du début du trait
+        x2, y2 : coordonnées de la fin du trait
+    
+    '''
+    rue.stroke_style = 'black'
+    rue.stroke_line(x1, y1, x2, y2)
