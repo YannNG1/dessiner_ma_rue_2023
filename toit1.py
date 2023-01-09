1fdf378a7a5bd57a3ff5e45f506db0f4c5894435
YannNG1 <yann.nunez-guajardo@eleves.ecmorlaix.fr>
2023-01-09 10:41:41 +0000

    a

---
+++ toit1.py
@@ -0,0 +1,27 @@
+# Dépendances
+from ma_rue import rue, affiche 
+
+# Définitions
+
+# Fonction toit1()
+def toit1(x, niveau):
+    '''
+    Dessine un triangle plein de couleur noir de 40 pixels de haut
+    et avec une base de 160 pixels 
+    Paramètres :
+        x : abcisse du centre du toit
+        niveau : numero du niveau (0 pour les rdc, ...)
+    '''
+    y = rue.height - niveau * 60 # ordonnée de la base du toit
+    rue.begin_path()
+    rue.move_to(x-70,y)  
+    rue.line_to(x,y-40)
+    rue.line_to(x+70, y)
+    rue.close_path()
+    rue.stroke()
+    rue.fill_style = "black"
+    rue.fill()
+if __name__ == '__main__':
+    # Tests
+    affiche(rue)
+    toit1(rue.width/2, 2)
