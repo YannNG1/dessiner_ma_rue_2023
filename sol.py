74f85aab4a9a292fe8b79f8fcf89dc0bf5f4a2c5
Tenduus <mateo.frerejacques@eleves.ecmorlaix.fr>
2023-01-12 07:28:38 +0000

    a

---
+++ sol.py
@@ -0,0 +1,22 @@
+# Dépendances
+from ma_rue import rue, affiche 
+from trait import trait 
+# Définitions
+
+# Fonction sol()
+def sol():
+    '''
+    Trace une ligne horizontale au niveau du sol de la rue
+    d'épaisseur 3 pixels et de longueur 760 pixels
+    centrée dans le canvas
+            
+    '''
+
+    y_sol = rue.height-1 # ordonnée du sol de la rue
+    trait(70,y_sol,-70,y_sol)
+
+
+if __name__ == '__main__':
+    # Tests
+    affiche(rue)
+    sol()
--- trait.py
+++ trait.py
@@ -1,3 +1,6 @@
+# Dépendances
+from ma_rue import rue, affiche
+
 # Définitions
 
 # Fonction trait()
@@ -11,3 +14,8 @@ def trait(x1,y1,x2,y2):
     '''
     rue.stroke_style = 'black'
     rue.stroke_line(x1, y1, x2, y2)
+
+if __name__ == '__main__':
+    # Tests
+    affiche(rue)
+    trait(50, 25, rue.width/2, rue.height/2)
