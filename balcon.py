b50acfc62d4094c57a0ac3cbfb7d63eea7463310
YannNG1 <yann.nunez-guajardo@eleves.ecmorlaix.fr>
2023-01-16 10:26:39 +0000

    a

---
+++ balcon.py
@@ -0,0 +1,28 @@
+# Dépendances
+from ma_rue import rue, affiche
+from rectangle import rectangle
+from trait import trait 
+
+# Définitions
+
+# Fonction balcon()
+def balcon(x,y):
+    '''
+    Dessine une porte fenêtre de largeur 30 pixels et 50 pixels en hauteur
+    avec une vitre de couleur 'Azure' le jour au contour noir,
+    devancé d'un balcon constitué de traits noirs d'épaisseur 3 pixels.
+    Paramètres :
+        x est l'abcisse du milieu de la base de la porte-fenetre
+        y est l'ordonnée du sol du niveau de la porte-fenetre
+    '''
+    # porte-fenetre
+    
+    
+    
+    # balcon
+    
+    
+    
+    # Tests
+    affiche(rue)
+    balcon(rue.width/2,rue.height)
