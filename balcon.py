155593298e00d4427df3d6962bd421ed18e030a7
YannNG1 <yann.nunez-guajardo@eleves.ecmorlaix.fr>
2023-01-16 10:32:51 +0000

    a

--- balcon.py
+++ balcon.py
@@ -1,5 +1,7 @@
 # Dépendances
 from ma_rue import rue, affiche
+# Dépendances
+from ma_rue import rue, affiche
 from rectangle import rectangle
 from trait import trait 
 
@@ -16,13 +18,16 @@ def balcon(x,y):
         y est l'ordonnée du sol du niveau de la porte-fenetre
     '''
     # porte-fenetre
-    
-    
+    rectangle(x, y, 30, 50, "Azure")
     
     # balcon
+    rue.line_width = 3
+    rue.stroke_rect(x-20, y, 40, -25)
+    for i in range(8):
+        trait(x-20+5*i, y, x-20+5*i, y-25)
+    rue.line_width = 1
     
-    
-    
+if __name__ == '__main__':
     # Tests
     affiche(rue)
     balcon(rue.width/2,rue.height)
