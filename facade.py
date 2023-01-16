d01d660d2574f45829338d3bf6795abe4dc1951e
Math2994 <mathieu.zavazzo@eleves.ecmorlaix.fr>
2023-01-16 09:57:35 +0000

    facade

---
+++ facade.py
@@ -0,0 +1,33 @@
+# Dépendances
+from ma_rue import rue, affiche
+from rectangle import rectangle
+
+# Définitions
+
+
+# Fonction
+def facade(x, couleur, niveau):
+    '''
+    Dessine un rectangle de 60 pixels de haut et 140 pixels de large
+    Paramètres :
+        x : abcisse du centre de la façade
+        couleur : couleur de la façade fixée par l'immeuble
+        niveau : numéro du niveau (0 pour les rdc, ...)
+    '''
+    couleur = couleur_aleatoire()
+    niveau = 0
+    y = rue.height - niveau * 60 # ordonnée de la base de la facade
+    for i in range(6):
+        niveau += i
+        y = rue.height - niveau * 60
+        rectangle(x, y, 140, 60, couleur )
+
+
+
+
+# Tests
+from couleur_aleatoire import couleur_aleatoire
+affiche(rue)
+couleur = couleur_aleatoire()
+for n in range(6) :
+    facade(rue.width/2, couleur, n)
