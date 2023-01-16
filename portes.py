1b8f0a59529939eff363f3360dd6de0ad1579c24
YannNG1 <yann.nunez-guajardo@eleves.ecmorlaix.fr>
2023-01-16 10:26:26 +0000

    a

--- portes.py
+++ portes.py
@@ -6,6 +6,15 @@ from math import pi
 from random import randint
 # Définitions
 
+'''
+    Dessine une porte de 50 pixels en largeur et 70 pixels en hauteur
+    La forme du haut de la porte est aléatoirement rectangulaire ou arrondi
+    La couleur pleine de remplissage est choisi aléatoirement parmi les couleurs HTML valides
+    Paramètres :
+        x est l'abcisse du milieu de la base de la porte
+        y est l'ordonnée du sol du niveau de la porte        
+'''   
+
 # Fonction portes()
 def portes(x,y):
     if randint(1,2) == 1:
@@ -20,16 +29,9 @@ def portes(x,y):
         rue.line_width = 1
         rue.stroke_arc(x, y -25, 13, pi, 0)
         rue.fill_rect(x - 11, y -25, 22, 10 )
-    '''
-    Dessine une porte de 50 pixels en largeur et 70 pixels en hauteur
-    La forme du haut de la porte est aléatoirement rectangulaire ou arrondi
-    La couleur pleine de remplissage est choisi aléatoirement parmi les couleurs HTML valides
-    Paramètres :
-        x est l'abcisse du milieu de la base de la porte
-        y est l'ordonnée du sol du niveau de la porte        
-    '''     
-     
-# Tests
-affiche(rue)
-for i in range(21) :
-    portes(0 + i * 40,rue.height)
+  
+if __name__ == '__main__':
+    # Tests
+    affiche(rue)
+    for i in range(21) :
+        portes(0 + i * 40,rue.height)
